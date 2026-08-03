b6cb71deeb7cd2ab78ea5acb18544ede4845829f
h1czvk0 <188614389+h1czvk0@users.noreply.github.com>
2026-08-04 07:37:58 +0800

    发布 3.9.21 自动连跳更新

--- slashco_updater.py
+++ slashco_updater.py
@@ -8,7 +8,7 @@ import tempfile
 from dataclasses import dataclass
 
 
-APP_VERSION = "3.9.20"
+APP_VERSION = "3.9.21"
 GITHUB_LATEST_RELEASE_API = "https://api.github.com/repos/h1czvk0/slsc/releases/latest"
 GITHUB_PROXY_PREFIXES = (
     "https://gh-proxy.org/",
